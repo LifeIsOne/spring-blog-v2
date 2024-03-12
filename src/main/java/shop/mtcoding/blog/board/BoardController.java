174e08f7ed6c845ebd627147024be14ece46654f
LifeIsOne <didtmdgh258@gmail.com>
2024-03-12 23:28:28 +0900

    공부 준비 완료~!

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,4 +1,4 @@
-package shop.mtcoding.blog.controller;
+package shop.mtcoding.blog.board;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
