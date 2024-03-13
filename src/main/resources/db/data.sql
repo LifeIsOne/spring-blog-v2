7d8e63a9dac27861d1fef0fc1e026142bd7bf04b
LifeIsOne <didtmdgh258@gmail.com>
2024-03-13 14:43:13 +0900

    step3 세팅

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,13 @@
--- INSERT INTO board_tb(title, content, username, created_at) VALUES ('제목1', '내용1', 'Kenneth', now());
--- INSERT INTO board_tb(title, content, username, created_at) VALUES ('제목2', '내용2', 'Minsu', now());
--- INSERT INTO board_tb(title, content, username, created_at) VALUES ('제목3', '내용3', 'Kim', now());
--- INSERT INTO board_tb(title, content, username, created_at) VALUES ('제목4', '내용4', 'Danny', now());
--- INSERT INTO board_tb(title, content, username, created_at) VALUES ('제목5', '내용5', 'Youngju', now());
--- INSERT INTO board_tb(title, content, username, created_at) VALUES ('제목6', '내용6', 'Lee', now());
+INSERT INTO user_tb (username, password, email, created_at) VALUES ('Kenneth', '1234', 'Kenneth@nate.com',now());
+INSERT INTO user_tb (username, password, email, created_at) VALUES ('Minsu', '1234', 'Minsu@nate.com',now());
+INSERT INTO user_tb (username, password, email, created_at) VALUES ('Kim', '1234', 'Kim@nate.com',now());
+INSERT INTO user_tb (username, password, email, created_at) VALUES ('Danny', '1234', 'Danny@nate.com',now());
+INSERT INTO user_tb (username, password, email, created_at) VALUES ('Yongju', '1234', 'Yongju@nate.com',now());
+INSERT INTO user_tb (username, password, email, created_at) VALUES ('Lee', '1234', 'Lee@nate.com',now());
+
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목1', '내용1', 1, now());
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목2', '내용2', 2, now());
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목3', '내용3', 3, now());
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목4', '내용4', 4, now());
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목5', '내용5', 5, now());
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목6', '내용6', 6, now());
